968570804f884b622103653e9a7f70181391226b
khalilrached <khalilrached058@gmail.com>
2023-10-05 23:22:04 +0100

    Delete dist directory

--- dist/utils/wrapper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function wrapper(fn) {
-    const handler = (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-    return handler;
-}
-exports.default = wrapper;
